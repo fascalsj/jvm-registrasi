adf4e2083b5d29c8c5b5cb19da7228def588312c
cron <fascalsj@gmail.com>
2020-03-07 10:28:53 +0700

    first commit

---
+++ src/test/java/com/jvm/aplikasiregistrasi/AplikasiRegistrasiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jvm.aplikasiregistrasi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplikasiRegistrasiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
